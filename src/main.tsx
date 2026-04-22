59a65aa84237dcc28b271f1007fe4a63d5c17379
andercapo0908-ship-it <andercapo0908@gmail.com>
2026-04-22 04:05:14 -0400

    Fix import statement for ErrorBoundary component

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { ErrorBoundary } from './ErrorBoundary';
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ErrorBoundary } from './ErrorBoundary.tsx' // Importando o que recriamos
