a6c812513ef5b21174d462ae62e733d2eaf00bfa
andercapo0908-ship-it <andercapo0908@gmail.com>
2026-04-22 06:19:59 -0400

    Improve root element check for ReactDOM rendering
    Refactor ReactDOM rendering logic to check for root element existence.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import { ErrorBoundary } from './ErrorBoundary';
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { ErrorBoundary } from './ErrorBoundary.tsx' // Importando o que recriamos
+import App from './App'
+import { ErrorBoundary } from './ErrorBoundary'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </React.StrictMode>
+  );
+}
